c82e44e0babcd8b28f8de1e23f9b5c1f2d182978
Talhakhan0819 <talhaniaz569@gmail.com>
2026-06-08 20:43:58 +0000

    conversion of dataTypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33"
+let score1 = "33abc"
+let score2 = null
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+let valueInNumber2 = Number(score2)
+
+
+console.log(typeof valueInNumber) 
+
+console.log(typeof valueInNumber1)
+console.log(valueInNumber1)
+
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2)
